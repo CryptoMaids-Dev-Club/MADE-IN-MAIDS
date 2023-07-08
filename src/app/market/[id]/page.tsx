1897724a4644655a86fc271f1c400b15eecf3897
POKENA7 <pokena191@gmail.com>
2023-07-08 22:56:37 +0900

    Update

--- src/app/market/[id]/page.tsx
+++ src/app/market/[id]/page.tsx
@@ -13,7 +13,7 @@ export default AssetDetail
 
 const getMetadata = async (id: string) => {
   const metadataId = [5, 1, 3, 6]
-
+  // ToDO: Modify Original Metadata URL
   const res = await fetch(
     `https://cryptomaids-art.s3.ap-northeast-1.amazonaws.com/market/metadata/${metadataId[Number(id)]}.json`
   )
