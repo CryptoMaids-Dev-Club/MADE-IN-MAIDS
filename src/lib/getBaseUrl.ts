c8d6597d053e53ce454a6ffedb53307bf75d6389
POKENA7 <pokena191@gmail.com>
2023-08-07 00:44:33 +0900

    getBaseUrl修正

--- src/lib/getBaseUrl.ts
+++ src/lib/getBaseUrl.ts
@@ -1,4 +1,4 @@
 export const getBaseUrl = () =>
   process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://market.cryptomaids.tokyo`
+    ? process.env.NEXT_PUBLIC_VERCEL_URL
     : `http://localhost:${process.env.PORT ?? 3000}`
