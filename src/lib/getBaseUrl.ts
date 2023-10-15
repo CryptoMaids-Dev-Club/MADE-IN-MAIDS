a3f8ebdeba47525d808b0ca95ac988e90248ba29
POKENA7 <pokena191@gmail.com>
2023-10-15 17:06:24 +0900

    t3-env対応

--- src/lib/getBaseUrl.ts
+++ src/lib/getBaseUrl.ts
@@ -1,2 +1,2 @@
-export const getBaseUrl = () => console.log(`https://${process.env.VERCEL_URL}`)
-process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : `http://localhost:${process.env.PORT ?? 3000}`
+export const getBaseUrl = () =>
+  process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : `http://localhost:${process.env.PORT ?? 3000}`
