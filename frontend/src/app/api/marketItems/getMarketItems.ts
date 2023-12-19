660d90a03621320eb5999987069d8750f2cd95a2
POKENA7 <pokena191@gmail.com>
2023-12-20 00:21:25 +0900

    Add console logs and error handling to getMarketItems function

--- frontend/src/app/api/marketItems/getMarketItems.ts
+++ frontend/src/app/api/marketItems/getMarketItems.ts
@@ -4,23 +4,28 @@ import type { MarketItemInfo } from '@/app/api/marketItems/marketItem'
 import 'server-only'
 
 export default async function getMarketItems() {
+  console.log('getMarketItems')
+
   try {
     const res = await fetch(`${getBaseUrl()}/api/marketItems`, {
       next: { revalidate: 600 },
     })
 
     if (!res.ok) {
+      console.log('not ok')
       throw new Error('Something went wrong!')
     }
 
     const marketItems = (await res.json()) as MarketItemInfo[]
 
     if (marketItems.length === 0) {
+      console.log('not found')
       notFound()
     }
 
     return marketItems
   } catch (e) {
+    console.log('error')
     console.error(e)
 
     return [] as MarketItemInfo[]
